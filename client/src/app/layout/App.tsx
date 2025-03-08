4517b8542a009dd844c3dbca83d9d6f7e9b562c2
Walter Wu <walterwu_001@hotmail.com>
2025-03-08 17:25:13 +1100

    End of 7

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+import { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
+import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import NavBar from "./NavBar";
+
+function App() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+  const darkMode = false;
+  const palleteType = darkMode ? 'dark': 'light'
+
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  });
+  
+  useEffect (() => {
+    fetch('https://localhost:5001/api/products')
+      .then(resopne => resopne.json())
+      .then(data => setProducts(data))
+  }, [])
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar />
+      <Box
+        sx={{
+          minHeight: '100vh',
+          background: darkMode ? '#121212' : '#eaeaea'
+        }}
+      >
+        <Container maxWidth='xl' sx={{mt: 14}}>      
+          <Catalog products={products}/>
+        </Container>
+      </Box>
+      <Container maxWidth='xl' sx={{mt: 14}}>      
+        <Catalog products={products}/>
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,13 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+export default function NavBar() {
+  return (
+    <AppBar position="fixed">
+        <Toolbar>
+            <Typography variant="h6">
+                RE-STORE
+            </Typography>            
+        </Toolbar>
+    </AppBar>
+  )
+}
